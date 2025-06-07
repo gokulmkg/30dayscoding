d220abd86d6d0b29eb5383d560177479ee44783e
GOKUL.M <144157488+gokulmkg@users.noreply.github.com>
2025-06-07 08:16:43 +0530

    Time: 11 ms (40.04%), Space: 49 MB (16.04%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+    Arrays.sort(g);
+    Arrays.sort(s);
+      int childIndex = 0;
+        int cookieIndex = 0;
+    
+ 
+    while(cookieIndex < s.length && childIndex < g.length) {
+      if(s[cookieIndex] >= g[childIndex]) {
+          childIndex++;
+      }
+     cookieIndex++;
+    }
+    return childIndex;
+}
+}
